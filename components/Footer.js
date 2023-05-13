b1150310953366de9107eada3c11e6bae11bf2af
whatdogdoin <81740717+whatdogdoin@users.noreply.github.com>
2023-05-13 13:42:01 -0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header className="hero__header">
+      <h1>Materials</h1>
+      <h2>Anode:</h2>
+      <h2>Materials for Anode</h2>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h2>Cathode:</h2>
+      <h2>Materials for cathode</h2>
+    </header>
+  );
+}
+
+export default Footer;
