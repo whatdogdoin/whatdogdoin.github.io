aaeef9156af8d51004ef15a6201f617ea2067e0c
whatdogdoin <81740717+whatdogdoin@users.noreply.github.com>
2023-05-13 13:57:24 -0700

    Create page.js

---
+++ page.js
@@ -0,0 +1,27 @@
+import Footer from ".//components/Footer";
+import Hero from ".//components/Hero";
+import ScrollSection from ".//components/ScrollSection";
+import SecondScrollSection from "./components/SecondScrollSection";
+import Reactions from "./components/Reactions";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ScrollSection />
+      <SecondScrollSection />
+      <Footer />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br><br></br>
+      <Reactions />
+    </>
+  );
+}
